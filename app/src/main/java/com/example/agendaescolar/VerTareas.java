b65c776160bb8349bff65f21ff1400a2b8ae07f3
José Luis Quiroz Ulin <josluisquirozulin@gmail.com>
2021-07-08 23:55:23 -0500

    Rediseñando listar tareas y materias

--- app/src/main/java/com/example/agendaescolar/VerTareas.java
+++ app/src/main/java/com/example/agendaescolar/VerTareas.java
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.agendaescolar.Adaptadores.ListaTareasAdapter;
 import com.example.agendaescolar.DB.dbTareas;
